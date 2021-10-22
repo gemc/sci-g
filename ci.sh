80c14376ccb5ed0f8e5f3b7e2c0c67dff512d657
Maria <zurek@anl.gov>
2021-10-21 20:20:34 -0700

    Change workflow to use different target jcards

--- ci.sh
+++ ci.sh
@@ -62,15 +62,16 @@ function check_overlaps {
 	fi
 }
 
-function run_all_targets {
-	run_geometry_gemc_target lh2
-	run_geometry_gemc_target c12
-}
-
 function run_all {
 	run_geometry_gemc examples/geometry/dosimeter example.py example.json
 	run_geometry_gemc examples/geometry/simple_flux example.py example.json
-	run_all_targets
+	run_geometry_gemc projects/clas12/targets targets.py target_lh2.jcard
+	run_geometry_gemc projects/clas12/targets targets.py target_c12.jcard
+}
+
+function run_targets {
+	run_geometry_gemc projects/clas12/targets targets.py target_lh2.jcard
+	run_geometry_gemc projects/clas12/targets targets.py target_c12.jcard
 }
 
 echo
@@ -83,8 +84,8 @@ if [ $# -eq 3 ]; then
 	echo "Running individual check" "$1" "$2" "$3"
 	run_geometry_gemc "$1" "$2" "$3"
 elif [ "$1" = "targets" ]; then
-	echo "Running all targets"
-	run_all_targets
+	echo "Running all target checks"
+	run_targets
 else
 	echo "Running all checks"
 	run_all
--- projects/clas12/targets/targets.py
+++ projects/clas12/targets/targets.py
@@ -11,24 +11,19 @@ from geometry import MAP_TARGET_TO_BUILDER
 def main():
     # Provides the -h, --help message
     desc_str = '   Will create the clas12 targets geometry\n'
-    parser = argparse.ArgumentParser(description=desc_str)
-    parser.add_argument('target',
-        choices=list(MAP_TARGET_TO_BUILDER.keys())
-    )
-    args = parser.parse_args()
-    target = args.target
 
     # Define GConfiguration name, factory and description. Initialize it.
     configuration = GConfiguration('clas12Target', 'TEXT', 'CLAS12 Targets')
     configuration.setVariation(target)
     configuration.init_geom_file()
 
-    # select the builder function based on the target passed as a command-line argument
-    build_geometry = MAP_TARGET_TO_BUILDER[target]
-    # run the selected builder function
-    build_geometry(configuration)
-    # print out the GConfiguration
-    configuration.printC()
+    # loop over all the defined builder finctions
+    for target_key, builder in MAP_TARGET_TO_BUILDER.items():
+        print(f"Building {target_key} target geometry")
+        # run the selected builder function
+        builder(configuration)
+        # print out the GConfiguration
+        configuration.printC()
 
 
 if __name__ == '__main__':
