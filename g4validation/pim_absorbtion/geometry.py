6c6db78db895c569c722379b1a51195c11c3d51d
Maurizio Ungaro <ungaro@jlab.org>
2022-11-14 15:22:20 -0500

    renamed typo in absorption

--- g4validation/pim_absorbtion/geometry.py
+++ g4validation/pim_absorbtion/geometry.py
@@ -1,26 +1,25 @@
 from gemc_api_geometry import GVolume
-import math
-
-def build_pim_absorbtion(configuration):
-	buildMotherVolume(configuration)
-	buildTarget(configuration)
-
-def buildMotherVolume(configuration):
-	gvolume = GVolume("vacuumDetector")
-	gvolume.make_tube(0, 60, 210, 0, 360, 'cm')
-	gvolume.material = 'G4_Galactic'
-	gvolume.color = 'FFAAAA4'
-	gvolume.digitization = 'flux'
-	gvolume.set_identifier('det', 1)
-	gvolume.publish(configuration)
-
-def buildTarget(configuration):
-	gvolume = GVolume("beamDump")
-	gvolume.mother = 'vacuumDetector'
-	gvolume.make_tube(0, 50, 200, 0, 360, 'cm')
-	gvolume.color = 'AAAAFF'
-	gvolume.material = 'G4_Al'
-	gvolume.publish(configuration)
 
 
+def build_pim_absorption(configuration):
+    build_mother_volume(configuration)
+    build_target(configuration)
 
+
+def build_mother_volume(configuration):
+    gvolume = GVolume("vacuumDetector")
+    gvolume.make_tube(0, 60, 210, 0, 360, 'cm')
+    gvolume.material = 'G4_Galactic'
+    gvolume.color = 'FFAAAA4'
+    gvolume.digitization = 'flux'
+    gvolume.set_identifier('det', 1)
+    gvolume.publish(configuration)
+
+
+def build_target(configuration):
+    gvolume = GVolume("beamDump")
+    gvolume.mother = 'vacuumDetector'
+    gvolume.make_tube(0, 50, 200, 0, 360, 'cm')
+    gvolume.color = 'AAAAFF'
+    gvolume.material = 'G4_Al'
+    gvolume.publish(configuration)
