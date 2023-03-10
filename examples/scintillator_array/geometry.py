43d1ecb1ad2022f732e9213c4d9f99b42057ea35
maureeungaro <maureeungaro@gmail.com>
2023-03-10 14:24:30 -0500

    scintillator_array example finalized

---
+++ examples/scintillator_array/geometry.py
@@ -0,0 +1,51 @@
+from math import cos, sin
+
+from gemc_api_geometry import *
+
+def buildGeometry(configuration):
+
+	# all dimensions are in cm
+	NBARS = 36             # number of bars in the array
+	dx1 = 3.0              # width of the paddle at the bottom
+	dx2 = 2.15              # width of the paddle at the top
+	dy  = 50               # length of the paddle
+	dz  = 5                # heigth of the paddle
+	theta0 = 360. / NBARS  # double the angle of one of the trapezoid sides
+	RMIN = 25              # inner radius of the array
+	gap  = 0.5             # gap to avoid overlaps
+
+
+	# first buld the other volume, it'a G4Tubs
+	gvolume = GVolume('tof')
+	gvolume.description = 'Time of flight'
+	gvolume.make_tube(RMIN - gap, RMIN + 2*dz + gap, dy+gap, 0, 360, 'cm')
+	gvolume.material = 'G4_AIR'
+	gvolume.color = 'f4f4ff5'
+	gvolume.style = 1
+	gvolume.publish(configuration)
+
+
+	# now build the bars
+	for i in range(NBARS):
+
+		theta_pl  = (i * theta0) * 3.1415926 / 180.
+		theta_rot = (i * theta0 + 90)
+
+		R = RMIN + dz
+
+		x = R * cos(theta_pl)
+		y = R * sin(theta_pl)
+		z = 0
+
+		gvolume = GVolume(f'bar_{i}')
+		gvolume.description = f'Scintillator bar_{i}'
+		gvolume.mother = 'tof'
+		gvolume.make_trapezoid(dx1, dx2, dy, dy, dz, 'cm')
+		gvolume.set_position(x, y, z, 'cm')
+		gvolume.set_rotation(90, theta_rot, 0, 'deg')
+		gvolume.material = 'G4_POLYSTYRENE'
+		gvolume.color = 'aa6666'
+		gvolume.digitization = 'flux'
+		gvolume.set_identifier('bar_id', i+1)
+		gvolume.publish(configuration)
+
---
+++ examples/scintillator_array/scintillator_array.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# imports: do not edit these lines
+import sys, os, argparse
+from gemc_api_utils import *
+from gemc_api_geometry import *
+
+# Provides the -h, --help message
+desc_str = "   Will create the simple_flux geometry\n"
+parser = argparse.ArgumentParser(description=desc_str)
+args = parser.parse_args()
+
+# Define GConfiguration name, factory and description. Initialize it.
+configuration = GConfiguration("scintillator_array", "TEXT", "scintillator_array geometry")
+configuration.init_geom_file()
+
+# build the geometry using the local geometry file
+from geometry import *
+buildGeometry(configuration)
+
+# print out the GConfiguration
+configuration.printC()
