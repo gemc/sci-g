20fa2c11540bb2601a3228bf16c23486de2c6e5d
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2023-04-25 15:15:55 -0400

    using system name and sqlite_file tags

--- examples/sqlite_db/sqlite_db.py
+++ examples/sqlite_db/sqlite_db.py
@@ -6,7 +6,7 @@ from materials import build_materials
 
 # Define GConfiguration: use TEXT factory.
 # Initialize geometry and materials files.
-txt_config = GConfiguration("sqlite_db", "TEXT", "water vapor cloud chamber with a target and a lead shield")
+txt_config = GConfiguration("cloudc", "TEXT", "water vapor cloud chamber with a target and a lead shield")
 txt_config.init_geom_file()
 txt_config.init_mats_file()
 
@@ -17,7 +17,7 @@ txt_config.printC()
 
 # Define GConfiguration: use SQLITE factory.
 # Set the sqlite file name and open the connection to it.
-sql_config = GConfiguration("sqlite_db", "SQLITE", "water vapor cloud chamber with a target and a lead shield")
+sql_config = GConfiguration("cloudc", "SQLITE", "water vapor cloud chamber with a target and a lead shield")
 sql_config.init_sqlite_file("sqlite_db.sqlite")
 
 # build geometry, materials and print out the GConfiguration
